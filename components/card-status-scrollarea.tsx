16b5737e955e26859e96afac91d44e7791faf12b
FelipeFVdev <feliep8@gmail.com>
2025-03-13 21:00:22 -0300

    fix: Missing Suspense boundary with useSearchParams

--- components/card-status-scrollarea.tsx
+++ components/card-status-scrollarea.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { CardStatusContent } from "@/components/card-status-content";
 import { CardStatusFooter } from "@/components/card-status-footer";
 import { CardContent } from "@/components/ui/card";
@@ -8,7 +9,9 @@ const CardStatusScrollArea = () => {
     <>
       <ScrollArea className="lg:h-[312px] h-72 mb-2">
         <CardContent className="grid h-full grid-cols-1 lg:grid-cols-3 gap-4 ">
-          <CardStatusContent />
+          <Suspense fallback={<div>Carregando...</div>}>
+            <CardStatusContent />
+          </Suspense>
         </CardContent>
       </ScrollArea>
       <CardStatusFooter />
