a1e5b3aef157cd0864a630c5b5a457010c703ea7
FelipeFVdev <feliep8@gmail.com>
2025-03-15 15:00:24 -0300

    chore: styles changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,39 @@
 import CardStatusScrollArea from "@/components/card-status-scrollarea";
+import StatusForm from "@/components/status-form";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { StatesProvider } from "./context/states-context";
 
-const Status = () => {
-  return <CardStatusScrollArea />;
-};
+export default function Status() {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Status Atual da COVID 19</CardTitle>
+          <CardDescription>
+            Consulta para visualizar o status atual da COVID-19: por estado,
+            data específica ou globalmente.
+          </CardDescription>
+        </CardHeader>
+        <StatesProvider>
+          <CardStatusScrollArea />
+        </StatesProvider>
+      </Card>
 
-export default Status;
+      <Card className="mb-8">
+        <CardHeader>
+          <CardTitle>Envie o status atual da COVID-19 no estado</CardTitle>
+          <CardDescription>
+            Forneça o relatório atualizado sobre a situação do estado em relação
+            à COVID-19.
+          </CardDescription>
+        </CardHeader>
+        <StatusForm />
+      </Card>
+    </>
+  );
+}
--- components/card-status-scrollarea.tsx
+++ components/card-status-scrollarea.tsx
@@ -7,7 +7,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 const CardStatusScrollArea = () => {
   return (
     <>
-      <ScrollArea className="lg:h-[312px] h-72 mb-2">
+      <ScrollArea className="h-72 mb-2">
         <CardContent className="grid h-full grid-cols-1 lg:grid-cols-3 gap-4 ">
           <Suspense fallback={<div>Carregando...</div>}>
             <CardStatusContent />
