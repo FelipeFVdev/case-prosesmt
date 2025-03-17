e277736d383495db260142fc3d18988cbf8cc49f
FelipeFVdev <feliep8@gmail.com>
2025-03-17 19:53:01 -0300

    chore: changes styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,14 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import { Toaster } from "@/components/ui/sonner";
+import StatusForm from "@/components/status-form";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { StatesProvider } from "./context/states-context";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +38,27 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} light antialiased lg:h-screen h-full flex items-center justify-center w-screen`}
       >
         <main className="mt-4 px-8 flex flex-col lg:grid lg:grid-cols-2 items-start justify-center gap-8">
-          {children}
+          <Card>
+            <CardHeader>
+              <CardTitle>Status Atual da COVID 19</CardTitle>
+              <CardDescription>
+                Consulta para visualizar o status atual da COVID-19: por estado,
+                data específica ou globalmente.
+              </CardDescription>
+            </CardHeader>
+            <StatesProvider>{children}</StatesProvider>
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <CardTitle>Envie o status atual da COVID-19 no estado</CardTitle>
+              <CardDescription>
+                Forneça o relatório atualizado sobre a situação do estado em
+                relação à COVID-19.
+              </CardDescription>
+            </CardHeader>
+            <StatusForm />
+          </Card>
         </main>
         <Toaster theme="light" position="top-right" richColors />
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,5 @@
 import CardStatusScrollArea from "@/components/card-status-scrollarea";
-import StatusForm from "@/components/status-form";
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { StatesProvider } from "./context/states-context";
 
 export default function Status() {
-  return (
-    <>
-      <Card>
-        <CardHeader>
-          <CardTitle>Status Atual da COVID 19</CardTitle>
-          <CardDescription>
-            Consulta para visualizar o status atual da COVID-19: por estado,
-            data específica ou globalmente.
-          </CardDescription>
-        </CardHeader>
-        <StatesProvider>
-          <CardStatusScrollArea />
-        </StatesProvider>
-      </Card>
-
-      <Card className="mb-8">
-        <CardHeader>
-          <CardTitle>Envie o status atual da COVID-19 no estado</CardTitle>
-          <CardDescription>
-            Forneça o relatório atualizado sobre a situação do estado em relação
-            à COVID-19.
-          </CardDescription>
-        </CardHeader>
-        <StatusForm />
-      </Card>
-    </>
-  );
+  return <CardStatusScrollArea />;
 }
